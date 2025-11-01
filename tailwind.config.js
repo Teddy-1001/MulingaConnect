2f4f9dccb93fd1de1d8ef6e7d2ecc21706915746
teddy <Williamsted1001@gmail.com>
2025-11-01 16:16:10 +0300

    cart section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Enables manual dark mode toggling
   content: [
     "./pages/**/*.{html,js}",
-    "./javascript/**/*.js"
+    "./javascript/**/*.js",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#013236',
-        accent: '#4EA771',
-        'light-green': '#C1E6BA',
-        background: '#EAF8E7',
-        text: '#013236'
+        green: "#4EA771",
+        coffee: "#2B1B0E",
+        cream: "#F9F6F1",
+        gold: "#D4AF37",
+        darkbrown: "#1A120B",
+        primary: "#013236",
+        accent: "#4EA771",
+        "light-green": "#C1E6BA",
+        background: "#EAF8E7",
+        text: "#013236",
       },
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif']
-      }
+        poppins: ['Poppins', 'sans-serif'],
+      },
     },
   },
   plugins: [],
-}
+};
